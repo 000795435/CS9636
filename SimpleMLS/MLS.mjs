e950e4ffed9b7c7fcdf6dbf58facfe7930ce4bdc
TYuUWO <69701775+TYuUWO@users.noreply.github.com>
2024-12-19 08:10:19 -0800

    Implemented Ratchet Trees (untested)
    ratchet tree functions implemented following the sample python code in Appendix C of IETF doc. Code has yet to be tested. Issue: functions should be incorporated into MLS functions, which should not handle groups from the class directly

--- SimpleMLS/MLS.mjs
+++ SimpleMLS/MLS.mjs
@@ -23,6 +23,8 @@ async function exportKey(key) {
 /**
  * MLS Library for Secure Messaging
  */
+
+
 class MLS {
   constructor() {
     this.groups = {}; // Store groups with their secrets, members, and message history
@@ -133,6 +135,133 @@ class MLS {
       privateKey: await exportKey(keyPair.privateKey),
     };
   }
+
+  //ratchet tree functions
+  /* The level of a node in the tree. Leaves are level 0, their parents
+  are level 1, etc. If a node's children are at different levels,
+  then its level is the max level of its children plus one. */
+  function level(node){
+    k = 0;
+    if(x & 0x01){return k;}
+    while((node >> k)&0x01){
+      k++;
+    }
+    return k;
+  }
+
+  //The number of nodes needed to represent a tree with n leaves.
+  function nodeWidth(n){
+    if(n===0){return 0;}
+    else{return 2*(n - 1) + 1;}
+  }
+
+  //The index of the root node of a tree with n leaves.
+  function root(n){
+    var w = nodeWidth(n);
+    return (1 << Math.log2(w)) - 1);
+  }
+
+  //The left child of an intermediate node.
+  function left(node){
+    var k = level(node);
+    if(k === 0){throw('leaf node has no children');}
+
+    return node ^ (0x01 << (k - 1));
+  }
+
+  //The right child of an intermediate node.
+  function right(node){
+    var k = level(node)
+    if k == 0{throw('leaf node has no children');}
+
+    return x ^ (0x03 << (k - 1));
+  }
+
+  //The parent of a node.
+  function parent(node, n){
+    if (node === root(n)){throw('root node has no parent');}
+
+    var = level(node);
+    var b = (node >> (k + 1)) & 0x01;
+    return (node | (1 << k)) ^ (b << (k + 1));
+  }
+
+  //The other child of the node's parent.
+  function sibling(node, n){
+    var p = parent(node, n);
+    if (node < p){return right(p);}
+    else{return left(p);}
+  }
+
+  //The direct path of a node, ordered from leaf to root.
+  function direct_path(node, n){
+    var r = root(n);
+    if (node == r){return [];}  //return a blank path (root of node is itself)
+
+    var d = [];
+    var pnode;
+    while node_in != r:
+        pnode = parent(node, n);
+        d.push(pnode); //append parent node
+    return d;
+  }
+
+  //The copath of a node, ordered from leaf to root.
+  function copath(node, n):
+    if node == root(n){return [];} //return blank path (root of node is itself)
+
+    var d = direct_path(node, n);
+    d.unshift(node);
+    d.pop();
+
+    //python version: return [sibling(y, n) for y in d]
+    var s = [];
+    for(var = 0; i<d.length; i++){
+      s.push(sibling(d[i]));
+    }
+    return s;
+  }
+
+  //The common ancestor of two nodes is the lowest node that is in the
+  //direct paths of both leaves.
+  function common_ancestor_semantic(nodex, nodey, n){
+    //intersection of arrays in js
+    var tempx = new Set([nodex]);
+    var tempPathx = new Set(direct_path(nodex, n))
+    var dx = tempx.union(tempPathx);
+    var tempy = new Set([nodey]);
+    var tempPathy = new Set(direct_path(nodey, n));
+    var dy = tempy.union(tempPathy));
+    var dxy = dx.intersection(dy);
+    if (dxy.size === 0){throw('failed to find common ancestor');}
+
+    var ldxy = [];
+    for(var val of dxy){
+      ldxy.push(level(val));
+    }
+    return Math.min(ldxy); //min(dxy, key=level)
+  }
+
+  //The common ancestor of two nodes is the lowest node that is in the
+  //direct paths of both leaves.
+  function common_ancestor_direct(nodex, nodey, _){
+    //Handle cases where one is an ancestor of the other
+    var lx = level(nodex)+1;
+    var ly = level(nodey)+1;
+    if ((lx <= ly) && (nodex>>ly === nodey>>ly)){return nodey;}
+    else if ((ly <= lx) && (x>>lx == y>>lx)){return x;}
+
+    //Handle other cases
+    var xn = nodex
+    var yn = nodey
+    var k = 0;
+    while (xn !== yn):
+       xn = xn >> 1;
+       yn = yn >> 1;
+       k++;
+    return (xn << k) + (1 << (k-1)) - 1;
+  }
+
 }
 
 export default MLS;
